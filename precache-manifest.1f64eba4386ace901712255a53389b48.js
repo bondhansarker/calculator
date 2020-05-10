c6859ecc990d559891f5fd757ed6d69096bf6e28
bondhansarker <bondhansarker100@gmail.com>
2020-05-10 19:08:32 +0600

    Updates

---
+++ precache-manifest.1f64eba4386ace901712255a53389b48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0a9156e3726e303ae18a1d59e11a72",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "6d2b4c83c981273675f7",
+    "url": "/calculator/static/css/main.00718248.chunk.css"
+  },
+  {
+    "revision": "2dc30cb1d17d497127aa",
+    "url": "/calculator/static/js/2.f848b2a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.f848b2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d2b4c83c981273675f7",
+    "url": "/calculator/static/js/main.d384303e.chunk.js"
+  },
+  {
+    "revision": "2d0c055e4a559390c19f",
+    "url": "/calculator/static/js/runtime-main.cec867ee.js"
+  }
+]);
